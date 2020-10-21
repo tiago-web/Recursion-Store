07d89f35b917f587c204c077d61f3adbb1668c16
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-10-21 17:06:39 -0400

    Add changes to products repository and services

--- server/src/modules/products/dtos/IUpdateProductDTO.ts
+++ server/src/modules/products/dtos/IUpdateProductDTO.ts
@@ -1,10 +1,10 @@
 import { IItem } from '../infra/mongoose/models/Product';
 
 export default interface IUpdateProductDTO {
-  name: string;
-  type: string;
-  categories: string[];
-  price: number;
-  description: String;
-  items: IItem[];
+  name?: string;
+  type?: string;
+  categories?: string[];
+  price?: number;
+  description?: String;
+  items?: IItem[];
 }
