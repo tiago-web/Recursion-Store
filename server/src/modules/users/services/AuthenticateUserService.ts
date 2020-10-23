2ac17852b332ab09eacc371de3a1b13108efa937
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-10-22 21:34:59 -0400

    Add AuthenticateUserService and token configuration

--- server/src/modules/users/services/AuthenticateUserService.ts
+++ server/src/modules/users/services/AuthenticateUserService.ts
@@ -1,7 +1,14 @@
-import { IUser } from "../infra/mongoose/models/User";
-import UserRepository from "../infra/mongoose/repositories/UsersRepository";
+import { sign } from "jsonwebtoken";
 
-const userRepository = new UserRepository();
+import authConfig from "@config/auth";
+import UsersRepository from "../infra/mongoose/repositories/UsersRepository";
+import BCryptHashProvider from "../providers/implementations/BCryptHashProvider";
+
+import { IUser } from "../infra/mongoose/models/User";
+import AppError from '@shared/errors/AppError'
+
+const usersRepository = new UsersRepository();
+const hashProvider = new BCryptHashProvider();
 
 interface IRequest {
   email: string;
@@ -15,7 +22,27 @@ interface IResponse {
 
 class AuthenticateUserService {
   public async execute({ email, password }: IRequest): Promise<IResponse> {
-    const user = await
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError("Incorrect email/password combination.", 401)
+    }
+
+    const passwordMatched = await hashProvider.compareHash(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("Incorrect email/password combination.", 401)
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return { token, user };
+
   }
 
 }
