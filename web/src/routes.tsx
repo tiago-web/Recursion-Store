4734e9a76b479411290b81f4ffa141400b8ec570
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-11-20 16:48:08 -0500

    Add myaccount page and update routes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -6,6 +6,7 @@ import ProductDetail from './pages/ProductDetail';
 import Products from './pages/Products';
 import ProductReview from './pages/ProductReview';
 import Cart from './pages/Cart';
+import MyAccount from './pages/User/MyAccount';
 
 const Routes: React.FC = () => {
   return (
@@ -15,6 +16,7 @@ const Routes: React.FC = () => {
       <Route component={ProductDetail} path="/product-detail" />
       <Route component={ProductReview} path="/product/review" />
       <Route component={Cart} path="/cart" />
+      <Route component={MyAccount} path="/user/myaccount" />
     </BrowserRouter>
   );
 };
