f8b2e2a4a3644946f09c867921daec84a97017f7
Sergio Pereira <sergiohgp@gmail.com>
2020-11-23 01:51:57 -0500

    Started cart page

--- web/src/pages/Cart/styles.ts
+++ web/src/pages/Cart/styles.ts
@@ -1,3 +1,12 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+export const CartSection = styled.section`
+  min-height: 700px;
+`;
