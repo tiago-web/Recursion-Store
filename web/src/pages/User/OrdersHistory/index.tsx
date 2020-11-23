d4c0846134c5a2932580c973dc0d1c449fa8188a
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-11-23 08:18:32 -0500

    include a route link to userSideBar and start ordershistory page

---
+++ web/src/pages/User/OrdersHistory/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserSideBar from '../../../components/UserSideBar';
+
+const OrdersHistory: React.FC = () => {
+  return (
+    <>
+      <UserSideBar />
+    </>
+  );
+};
+
+export default OrdersHistory;
