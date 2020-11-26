744ef22d0f922125d8a5476b4d2904d704ed4a37
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-11-25 23:22:43 -0500

    Update product images to github

--- web/src/pages/Products/index.tsx
+++ web/src/pages/Products/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
-import ProductsSideBar from './components/ProductsSideBar';
+
 import ProductList from './components/ProductList';
 import { ProductsFilterProvider } from '../../contexts/ProductsFilterContext';
 
 import { ProductsContainer } from './styles';
+import ProductsSideBar from './components/ProductsSideBar';
 
 const Products: React.FC = () => {
   return (
