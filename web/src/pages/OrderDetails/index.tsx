f861a1010f8c8b11694d9ab219139028a59c035b
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-11-27 11:04:40 -0500

    Create OrderDetails page

---
+++ web/src/pages/OrderDetails/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Grid, Paper } from '@material-ui/core';
+// import { useStyles } from './styles';
+
+const OrderDetails: React.FC = () => {
+  return <></>;
+};
+
+export default OrderDetails;
