fe6e0dfa8f71241d57fcca12a0f691a85e1ae111
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-12-07 12:53:13 -0500

    Add handleSizeSelected and handleSizeDelete props and added them to the corresponding section on SizeTag component.

--- web/src/pages/Admin/AddEditProduct/components/SizeTag/index.tsx
+++ web/src/pages/Admin/AddEditProduct/components/SizeTag/index.tsx
@@ -1,19 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import ClearIcon from '@material-ui/icons/Clear';
 import { Container, Size } from './styles';
+import { TSize } from '../../Atoms';
 
 interface SizeTagProps {
   sizeTag: string;
   quantity: number;
+  handleSizeSelected(sizeToUpdate: TSize): void;
+  handleSizeDelete(sizeTag: string): void;
 }
 
-const SizeTag: React.FC<SizeTagProps> = ({ sizeTag, quantity }) => {
+const SizeTag: React.FC<SizeTagProps> = ({
+  sizeTag,
+  quantity,
+  handleSizeSelected,
+  handleSizeDelete,
+}) => {
   return (
     <Container>
-      <Size>
+      <Size onClick={() => handleSizeSelected({ sizeTag, quantity })}>
         {sizeTag}({quantity})
       </Size>
-      <button type="button">
+      <button type="button" onClick={() => handleSizeDelete(sizeTag)}>
         <ClearIcon />
       </button>
     </Container>
