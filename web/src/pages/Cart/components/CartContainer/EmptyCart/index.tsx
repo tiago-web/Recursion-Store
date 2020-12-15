4cbb8d1f06e5a80eb976d7beaaf161b5f854843f
Sergio Pereira <sergiohgp@gmail.com>
2020-12-15 16:04:26 -0500

    minor changes

--- web/src/pages/Cart/components/CartContainer/EmptyCart/index.tsx
+++ web/src/pages/Cart/components/CartContainer/EmptyCart/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from '../../../../../components/Button';
@@ -6,15 +6,17 @@ import Button from '../../../../../components/Button';
 import { Container } from './styles';
 
 const EmptyCart: React.FC = () => {
+  const handleBestSellersClick = useCallback(() => {
+    window.scrollTo(0, document.body.scrollHeight);
+  }, []);
+
   return (
     <>
       <Container>
         <div className="emptyCart">
           <strong>You have no items in your bag.</strong>
           <span>Don't know where to start? Here's our best sellers</span>
-          <Button>
-            <Link to="/products">Best Sellers</Link>
-          </Button>
+          <Button onClick={handleBestSellersClick}>Best Sellers</Button>
         </div>
       </Container>
     </>
--- web/src/pages/ProductDetail/components/ProductReviewsContainer/ProductReviewBody/index.tsx
+++ web/src/pages/ProductDetail/components/ProductReviewsContainer/ProductReviewBody/index.tsx
@@ -1,6 +1,7 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
-import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import { Container, NoReviewsContainer } from './styles';
 import { Review as IReview } from '../../..';
 import Review from './Review';
 import Button from '../../../../../components/Button';
@@ -19,43 +20,70 @@ const ProductReviewBody: React.FC<ProductReviewBodyProps> = ({
   productId,
 }) => {
   const [loadMore, setLoadMore] = useState(3);
+  const [isReview, setIsReview] = useState(false);
+  const [title, setTitle] = useState('Most Relevant');
+
+  useEffect(() => {
+    if (reviews.length <= 0) {
+      setIsReview(false);
+      setTitle('No Reviews yet');
+    } else {
+      setIsReview(true);
+      setTitle('Most Relevant');
+    }
+  }, [reviews]);
 
   const handleLoadMoreReviews = useCallback(() => {
     setLoadMore(loadMore + 3);
   }, [loadMore]);
 
   return (
-    <Container>
-      {showAllReviews ? (
-        <>
-          {reviews.slice(0, loadMore).map(review => (
-            <Review
-              key={review._id}
-              review={review}
-              handleDeleteReview={handleDeleteReview}
-              productId={productId}
-            />
-          ))}
-          {reviews.length > loadMore && (
-            <Button
-              style={{ marginTop: '48px' }}
-              onClick={handleLoadMoreReviews}
-            >
-              Load 3 more
-            </Button>
-          )}
-        </>
+    <>
+      {isReview ? (
+        <Container>
+          {showAllReviews ? (
+            <>
+              {reviews.slice(0, loadMore).map(review => (
+                <Review
+                  key={review._id}
+                  review={review}
+                  handleDeleteReview={handleDeleteReview}
+                  productId={productId}
+                />
+              ))}
+              {reviews.length > loadMore && (
+                <Button
+                  style={{ marginTop: '48px' }}
+                  onClick={handleLoadMoreReviews}
+                >
+                  Load 3 more
+                </Button>
+              )}
+            </>
+          ) : (
+              <>
+                <h1>{title}</h1>
+                <Review
+                  review={reviews[0]}
+                  handleDeleteReview={handleDeleteReview}
+                  productId={productId}
+                />
+              </>
+            )}
+        </Container>
       ) : (
           <>
-            <h1>Most Relevant</h1>
-            <Review
-              review={reviews[0]}
-              handleDeleteReview={handleDeleteReview}
-              productId={productId}
-            />
+            <NoReviewsContainer>
+              <h2>{title}</h2>
+              <span>Be the first to review</span>
+              <span>
+                Click on the write a review button above an tell us how you feel
+                about this product.
+            </span>
+            </NoReviewsContainer>
           </>
         )}
-    </Container>
+    </>
   );
 };
 
